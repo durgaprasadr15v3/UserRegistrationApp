8f7fbf1aa66c533963e6adcd9b4a79a2294faeaf
KATTA DURGA PRASAD <99981513+durgaprasadr15v3@users.noreply.github.com>
2024-05-30 23:44:07 +0530

    Update SecondMiniProjectApplication.java

--- src/main/java/com/example/secondminiproject/SecondMiniProjectApplication.java
+++ src/main/java/com/example/secondminiproject/SecondMiniProjectApplication.java
@@ -10,6 +10,10 @@ public class SecondMiniProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SecondMiniProjectApplication.class, args);
 	}
+
+	public void addmethod()
+	{
+	}
   
          void newupdate()
 {
